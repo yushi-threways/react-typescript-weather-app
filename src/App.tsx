0aeceef6d0e5282bd8bee17495112b79c6670391
yushi-threways <seki@threways.co.jp>
2021-10-07 02:06:45 +0900

    Title componentを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Title from './components/Title';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="test">
+      <Title />
     </div>
   );
 }
