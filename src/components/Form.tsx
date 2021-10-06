fe6bd74d3efc0c3a87ddfe488fe8ab6c36293d3f
yushi-threways <seki@threways.co.jp>
2021-10-07 02:44:42 +0900

    天気apiを受け取りjsonに変換する処理を追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,19 @@ import { useState } from "react";
 const Form = () => {
     // city ユーザーが入力してしたデータを保管する
     const [city, setCity] = useState<string>("");
-
+    const getWeather = (e: any) => {
+        // formの本来の動きを止める
+        e.preventDefault();
+        
+        fetch(
+            "https://api.weatherapi.com/v1/current.json?key=701cb2f152774df6a31172726210610&q=London&aqi=no"
+        ).then(
+            res => res.json()
+            // jsonへ書き換え
+        ).then(data => console.log(data));
+        
+        ; 
+    };
     return(
         <form>
             <input 
@@ -12,8 +24,7 @@ const Form = () => {
             placeholder="都市名" 
             onChange={e => setCity(e.target.value)}
             />
-            {city}
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 }
